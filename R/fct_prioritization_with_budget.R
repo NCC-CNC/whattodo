731de6d1b433180d630f1cea5d27cafc7b46506e
Dan Wismer <daniel.s.wismer@gmail.com>
2025-03-13 11:02:08 -0600

    optimize on either norm or raw pu data, evaluate solution on raw pu data

--- R/fct_prioritization_with_budget.R
+++ R/fct_prioritization_with_budget.R
@@ -11,7 +11,8 @@ NULL
 #'
 #' @param action_ids `character` action identifiers.
 #'
-#' @param pu_data `dat.frame` containing planning unit data.
+#' @param pu_data `list` containing raw planning unit data as `data.frame` and
+#' cost normalized planning unit data as either `data.frame` or `NULL`.
 #'
 #' @param status_data `dat.frame` containing status data.
 #'
@@ -61,7 +62,9 @@ prioritization_with_budget <- function(site_ids,
     assertthat::noNA(feature_ids),
     is.character(action_ids),
     assertthat::noNA(action_ids),
-    inherits(pu_data, "data.frame"),
+    is.list(pu_data),
+    inherits(pu_data$raw_data, "data.frame"),
+    inherits(pu_data$norm_data, "data.frame") | is.null(pu_data$norm_data),
     inherits(status_data, "data.frame"),
     inherits(zone_data, "ZonesCharacter"),
     inherits(goal_data, "data.frame"),
@@ -87,10 +90,17 @@ prioritization_with_budget <- function(site_ids,
   # prepare target data
   target_data <-
     goal_data[, c("feature", "zone", "type", "sense", "target"), drop = FALSE]
+  
+  # set problem data to use raw or normalized costs to solve
+  if (is.null(pu_data$norm_data)) {
+    prb_pu_data <- pu_data$raw_data
+  } else {
+    prb_pu_data <- pu_data$norm_data
+  }  
 
   # generate prioritization
   prb <-
-    prioritizr::problem(pu_data, zone_data, as.character(cost_names)) %>%
+    prioritizr::problem(prb_pu_data, zone_data, as.character(cost_names)) %>%
     prioritizr::add_min_shortfall_objective(budget = max(budget, 1e-5)) %>%
     prioritizr::add_feature_weights(matrix(weight_data[[2]], ncol = 1)) %>%
     prioritizr::add_manual_targets(target_data) %>%
@@ -104,6 +114,7 @@ prioritization_with_budget <- function(site_ids,
       prb %>%
       prioritizr::add_manual_locked_constraints(locked_data)
   }
+  
   sol <- prioritizr::solve.ConservationProblem(prb)
 
   # summarize results
@@ -111,7 +122,7 @@ prioritization_with_budget <- function(site_ids,
     site_ids = site_ids,
     feature_ids = feature_ids,
     action_ids =  action_ids,
-    pu_data = pu_data,
+    pu_data = pu_data$raw_data, # evaluate solution based on raw data
     status_data = status_data,
     zone_data = zone_data,
     goal_data = goal_data,
